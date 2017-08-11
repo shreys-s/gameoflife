d4d656c2ea53fcfe040b19cf9e3dd094a0bc2618
shreys-s <shrey_s@hcl.com>
2017-08-11 11:04:32 +0530

    added Kunal

--- DB/create_data.sql
+++ DB/create_data.sql
@@ -1,5 +1,5 @@
 INSERT INTO people 
-VALUES (1, 'Dave', 'Canda', 'Mr', date'1960-05-01');
+VALUES (1, 'kunal', 'US', 'Mr', date'1990-05-01');
 INSERT INTO people (person_id, given_name, family_name, title) 
 VALUES (2, 'Simon', 'Fox', 'Mr');
 INSERT INTO people (person_id, given_name, family_name, title) 
