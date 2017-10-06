abb90e1bedfb2c1f5f43871524b2372658a0dfec
shreys-s <shrey_s@hcl.com>
2017-10-06 12:35:56 +0530

    create_data.sql

--- DB/create_data.sql
+++ DB/create_data.sql
@@ -1,9 +1,9 @@
 INSERT INTO people 
-VALUES (1, 'Shrey', 'USA', 'Mr', date'2000-05-01');
+VALUES (1, 'Shrey1', 'USA', 'Mr', date'2000-05-01');
 INSERT INTO people (person_id, given_name, family_name, title) 
-VALUES (2, 'Vikas', 'Fox', 'Mr');
+VALUES (2, 'Shruti', 'Fox', 'Mr');
 INSERT INTO people (person_id, given_name, family_name, title) 
-VALUES (3, 'Shru', 'TestUser', (SELECT 'Mr' FROM dual));
+VALUES (3, 'Anshl', 'TestUser', (SELECT 'Mr' FROM dual));
 INSERT INTO people (person_id, given_name, family_name, title) 
   WITH names AS ( 
     SELECT 4, 'Shrey',     'Sangal',      'Mr'    FROM dual UNION ALL 
